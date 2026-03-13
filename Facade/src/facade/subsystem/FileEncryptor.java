938e19b1efe43dda7af3332516e47ab003f89a91
juanx199 <132611235+juanx199@users.noreply.github.com>
2026-03-13 15:24:10 -0500

    Implement character shift encryption in FileEncryptor
    Updated encryption and decryption methods to use a character shift algorithm.

--- Facade/src/facade/subsystem/FileEncryptor.java
+++ Facade/src/facade/subsystem/FileEncryptor.java
@@ -9,12 +9,22 @@ package facade.subsystem;
  * @author JUANCA
  */
 public class FileEncryptor {
+    private static final int SHIFT = 10;
     public String encrypt(String data){
-        return "[Encriptado]" + data + "[Encriptado]";
+        System.out.print("[FileEncryptor] Cifrando..");
+        StringBuilder encrypted = new StringBuilder();
+        for (char c : data.toCharArray()){
+            encrypted.append((char)(c + SHIFT));
+        }
+        return encrypted.toString();
     }
-    
+
     public String decrypt (String data){
-        System.out.println("Desencriptando ...");
-        return data.replace("[Encriptado]", "");
+        System.out.println("[FileEncryptor] Desencritpando ...");
+        StringBuilder decrypted = new StringBuilder();
+        for (char c : data.toCharArray()){
+            decrypted.append((char) (c-SHIFT));
+        }
+        return decrypted.toString();
     }
 }
